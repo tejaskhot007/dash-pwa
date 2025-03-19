f592e9bfb0087cc259be631747f4b033b43c9c65
welcome tejas <tejaskhot4115@gmail.com>
2025-03-19 11:56:54 +0530

    Fix manifest.json, add icons and screenshots, update sw.js for PWA

--- static/sw.js
+++ static/sw.js
@@ -7,18 +7,25 @@ const urlsToCache = [
   '/_dash-dependencies'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('dash-pwa-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/static/icon-144.png',
+                '/static/icon-512.png',
+                '/static/manifest.json'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
